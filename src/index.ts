5e36053975ee6a703fdf9f10080030951c598223
ArthurBurle <arthurburle@gmail.com>
2021-01-15 16:03:24 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+/* const numbersCollection = new NumbersCollection([11, 3, -4, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data); */
+
+/* const charactersCollection = new CharactersCollection('ktqefARGWRweeFWEFWE');
+charactersCollection.sort();
+console.log(charactersCollection.data); */
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(1);
+
+linkedList.sort();
+linkedList.print();
